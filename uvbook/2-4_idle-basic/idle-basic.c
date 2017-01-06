183d77d1876e0cfafd12c2dbe4ab990ab3258572
Wataru Shito <wataru007jp@gmail.com>
2017-01-07 01:26:59 +0900

    Added data passing example.

--- uvbook/2-4_idle-basic/idle-basic.c
+++ uvbook/2-4_idle-basic/idle-basic.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include "uv.h"
 
-void wait_for_a_while(uv_idle_t* handle) {
-  static int64_t counter = 0;
+typedef struct {
+    const u_int64_t max;
+    u_int64_t counter;
+} user_data_t;
 
-  if (++counter >= 10e6) {
-    handle->data = &counter; // pass the user data
-    uv_idle_stop(handle);
-  }
+void wait_for_a_while(uv_idle_t* handle) {
+    user_data_t* user_data = (user_data_t *) (handle->data);
+    
+    if (++(user_data->counter) >= user_data->max) {
+        uv_idle_stop(handle);
+    }
 }
 
 int main() {
+    user_data_t user_data = { 10e6, 0 }; // max=2, counter=0
+    
     uv_idle_t idler;
-
-    printf("Idling...\n");
-
+    idler.data = &user_data;
+    
+    printf("Idling %lld times...\n", user_data.max);
+    
     uv_idle_init(uv_default_loop(), &idler);
     uv_idle_start(&idler, wait_for_a_while);
-
+    
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
-
+    
     uv_loop_close(uv_default_loop());
-
+    
     // print the user data
-    printf("Counter = %lld\n", *(int64_t*)idler.data);
+    printf("Counter = %lld\n", ((user_data_t*) (idler.data))->counter);
     
     return 0;
 }
