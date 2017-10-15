ac17359dc8786910a289784d22d1c3ef0275a0c9
Wataru Shito <wataru007jp@gmail.com>
2017-10-15 14:29:53 +0900

    first implementation.

---
+++ lisp/lack/middleware-mount.lisp
@@ -0,0 +1,37 @@
+;;
+;; See http://diary.wshito.com/comp/lisp/clack/lack-middleware-mount/
+;; for explanation.
+;;
+
+(require 'clack)
+(require 'lack)
+
+;;
+;; Echos current path-info
+;;
+(setf my-echo
+      (lambda (env)
+	`(200 (:content-type "text/plain") ,(list (getf env :path-info)))))
+;;
+;; Creates Lack Application
+;;
+(defparameter *app*
+  (lack:builder
+    (:mount "/echo" my-echo)
+    (:mount "/hello"
+	    (lambda (env)
+	      (declare (ignore env))
+	      '(200 (:content-type "text/plain") ("Hello world! 2"))))
+    (lambda (env)
+      (declare (ignore env))
+      '(404 (:content-type "text/plain") ("Not found, mate!")))))
+;;
+;; Starts the Web server
+;;
+(defparameter *handler*
+  (clack:clackup *app*))
+
+;;
+;; Stops the Web server
+;;
+; (clack:stop *handler*)
